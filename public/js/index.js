4d34a42da6dc30394b042568436f337ac78bfcec
BarneyAtAldi2 <139533726+BarneyAtAldi2@users.noreply.github.com>
2024-03-07 19:38:01 -0500

    .js

--- public/js/index.js
+++ public/js/index.js
@@ -79,6 +79,21 @@ if (!cdn) {
     });
 }
 
+function abtab() {
+  var url = document.location.href
+  var win = window.open();
+  win.document.body.style.margin = '0';
+  win.document.body.style.height = '100vh';
+  var iframe = win.document.createElement('iframe');
+  iframe.style.border = 'none';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.margin = '0';
+  iframe.src = url;
+  window.location.replace("https://classroom.google.com/h")
+  win.document.body.appendChild(iframe);
+}
+
 const instance = encodeURIComponent(origin.replace(location.origin, ''));
 
 window.onload = () => {
--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,20 @@
 //
 // If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io, or discord: 3kh0_#6969
 
-console.warn(
-  "%cNote!",
-  "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "Barney is Cool"
-);
+function script(text) {
+  console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", text);
+}
+
+// ====================================
+// SCRIPT INJECTION
+// ====================================
+const gogascript27 = document.createElement("script");
+gogascript27.setAttribute("async", "");
+gogascript27.setAttribute("src", "https://cdn.counter.dev/script.js");
+const inlinegogascript843 = document.createElement("script");
+inlinegogascript843.innerHTML = `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-98DP5VKS42');`;
+document.head.append(gogascript27, inlinegogascript843);
+script("Injected script 1/4 (counter.dev)");
